aa28a2b35a75f2fadefa620f0a8ab64fcea1173d
Manhnguyenx <137817499+Manhnguyenx@users.noreply.github.com>
2024-11-20 23:14:46 +0700

    ProjectTien

---
+++ ProjectTiente/ProjectTiente/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap so tien USD can chuyen doi: ");
+        int Rate = 23000;
+
+        int usdAmount;
+      
+        Console.ReadLine(usdAmount);
+
+        int vndAmount = usdAmount * Rate;
+
+        Console.WriteLine($"So tien duoc quy doi sang la: {vndAmount} VND." );
+    }
+}
+    
